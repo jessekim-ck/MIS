fa33b5e0b1913459906e51f00782874184f41512
jessekim-ck <jessekim.ck.94@gmail.com>
2020-04-30 17:40:15 +0900

    Added dark/light mode

---
+++ script.js
@@ -0,0 +1,19 @@
+
+window.addEventListener("load", () => {
+    load_stylesheet();
+})
+
+function load_stylesheet() {
+    const style_sheet = localStorage.getItem("stylesheet") || "light.css";
+    document.getElementById("stylesheet").setAttribute("href", style_sheet);
+}
+
+function set_dark_mode() {
+    localStorage.setItem("stylesheet", "dark.css");
+    load_stylesheet();
+}
+
+function set_light_mode() {
+    localStorage.setItem("stylesheet", "light.css");
+    load_stylesheet();
+}
